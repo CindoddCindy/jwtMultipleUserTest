6d1fba23240de0b3e269e51b98f9ca6d2e644589
CindoddCindy <cindodcindy@gmail.com>
2020-12-04 15:51:00 +0700

    push folder

---
+++ demo/src/test/java/jwtMultipleUserTest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package jwtMultipleUserTest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
